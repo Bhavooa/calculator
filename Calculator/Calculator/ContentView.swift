a190064011df11c929ee41d3dcab6c82f3172c24
Bhavya Rajput <bhavyarajput17@gmail.com>
2024-10-15 21:25:37 -0700

    Made the base thing
    Met MVP

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -9,47 +9,102 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State private var displayText = "0"
+    @State private var currentInput = ""
+    @State private var storedValue:Double? = 0
+    @State private var currentOperator:String?
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack {
+            Text(displayText)
+                .font(.system(size: 80))
+            
+            CalculatorButtonRow(buttons: ["7", "8", "9", "/"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["4", "5", "6", "*"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["1", "2", "3", "-"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["0", ".", "=", "+"], action: buttonTap)
+            CalculatorButtonRow(buttons: ["C"], action: {button in clearButton()})
+            
+            
+        }
+        .padding()
+    }
+    
+    
+    
+    
+    func buttonTap(_ button:String){
+        switch button {
+        case "+", "-", "*", "/":
+            currentOperator = button
+            storedValue = Double(currentInput)
+            currentInput = ""
+        case "=":
+            if let operatorType = currentOperator,
+               let storedValue = storedValue,
+               let currentValue = Double(currentInput) {
+                
+                let result: Double
+                
+                switch operatorType{
+                case "+":
+                    result = storedValue + currentValue
+                case "-":
+                    result = storedValue - currentValue
+                case "*":
+                    result = storedValue * currentValue
+                case "/":
+                    result = storedValue / currentValue
+                default:
+                    fatalError("Unkown Operator")
                 }
-                .onDelete(perform: deleteItems)
+                currentInput = "\(result)"
+                currentOperator = nil
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            
+        case ".":
+            currentInput += button
+        default:
+            currentInput += button
+            
+        }
+        
+        if currentInput.isEmpty{
+            displayText = "0"
+        } else {
+            displayText = currentInput
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
+    
+    func clearButton() {
+        currentInput = ""
+        storedValue = nil
+        currentOperator = nil
+        displayText = "0"
     }
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+
+struct CalculatorButtonRow: View{
+    
+    
+    let buttons:[String]
+    let action:(String) -> Void
+    let buttonSize = (UIScreen.main.bounds.width - 5 * 12) / 4
+    
+    var body: some View {
+        HStack(spacing: 12){
+            ForEach(buttons, id:\.self) {buttonLable in
+                Button {
+                    action(buttonLable)
+                } label: {
+                    Text(buttonLable)
+                        .font(.title)
+                        .frame(width: buttonSize, height: buttonSize)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(buttonSize)
+                }
             }
         }
     }
